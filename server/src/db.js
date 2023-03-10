f936ff1f304d50e7af2ff50f400eeb4dd32be0e5
David Carroll <dcarroll2223@gmail.com>
2023-03-10 11:31:44 -0600

    update connection

--- server/src/db.js
+++ server/src/db.js
@@ -1,13 +1,11 @@
-import { db } from './config.js';
 import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: db.user,
-  password: db.password,
-  host: db.host,
-  port: db.port,
-  database: db.database,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
